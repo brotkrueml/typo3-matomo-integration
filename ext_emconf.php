0da699e5b52940187f87611a0f3bf9cfaa4584c8
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-08-10 19:48:27 +0200

    [TASK] Add PHP constraint

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,6 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.15-11.5.99',
+            'php' => '7.4.0-0.0.0',
         ],
         'conflicts' => [],
         'suggests' => [],
