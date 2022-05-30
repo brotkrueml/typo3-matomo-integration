5e2e56d327bb8bdeb366eb5ea071110ffbb6b221
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2022-05-30 18:40:39 +0200

    [TASK] Update brotkrueml/coding-standards to version 2.0.0

--- ecs.php
+++ ecs.php
@@ -2,10 +2,9 @@
 
 declare (strict_types=1);
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (ECSConfig $config): void {
     $header = <<<HEADER
 This file is part of the "matomo_integration" extension for TYPO3 CMS.
 
@@ -13,27 +12,19 @@ For the full copyright and license information, please read the
 LICENSE.txt file that was distributed with this source code.
 HEADER;
 
-    $containerConfigurator->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
+    $config->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
 
-    $services = $containerConfigurator->services();
-    $services->set(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class)
-        ->call('configure', [[
-            'comment_type' => 'comment',
-            'header' => $header,
-            'separate' => 'both',
-        ]]);
-
-    $parameters = $containerConfigurator->parameters();
-    $parameters->set(
-        Option::PATHS,
-        [
-            __DIR__ . '/Classes',
-            __DIR__ . '/Configuration',
-            __DIR__ . '/Tests',
-        ]
-    );
-
-    $parameters->set(Option::SKIP, [
+    $config->paths([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+    ]);
+    $config->ruleWithConfiguration(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class, [
+        'comment_type' => 'comment',
+        'header' => $header,
+        'separate' => 'both',
+    ]);
+    $config->skip([
         \PhpCsFixer\Fixer\ClassNotation\ClassAttributesSeparationFixer::class => [
             __DIR__ . '/Classes/Extension.php',
         ],
@@ -41,5 +32,4 @@ HEADER;
             __DIR__ . '/Configuration/SiteConfiguration/Overrides/sites.php',
         ],
     ]);
-
 };
