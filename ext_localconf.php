a5452fadc1312825c46d43f3948ca18232168e75
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2024-02-08 19:13:41 +0100

    chore: update rector to version 1.0

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Brotkrueml\MatomoIntegration\Hooks\PageRenderer\TrackingCodeInjector;
 
-defined('TYPO3') or die();
+defined('TYPO3') || die();
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-postTransform'][] =
     TrackingCodeInjector::class . '->execute';
--- rector.php
+++ rector.php
@@ -5,47 +5,33 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\PreferPHPUnitThisCallRector;
-use Rector\PHPUnit\CodeQuality\Rector\ClassMethod\ReplaceTestAnnotationWithPrefixedFunctionRector;
 use Rector\PHPUnit\Set\PHPUnitSetList;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddParamTypeFromPropertyTypeRector;
-use Rector\TypeDeclaration\Rector\FunctionLike\AddReturnTypeDeclarationFromYieldsRector;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromAssignsRector;
-use Rector\ValueObject\PhpVersion;
 
-return static function (RectorConfig $config): void {
-    $config->phpVersion(PhpVersion::PHP_81);
-
-    $config->sets([
-        LevelSetList::UP_TO_PHP_81,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_100,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-    ]);
-
-    $config->autoloadPaths([
-        __DIR__ . '/.Build/vendor/autoload.php',
-    ]);
-    $config->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/Classes',
         __DIR__ . '/Tests',
-    ]);
-    $config->skip([
+    ])
+    ->withPhpSets()
+    ->withAutoloadPaths([
+        __DIR__ . '/.Build/vendor/autoload.php',
+    ])
+    ->withImportNames(
+        importShortClasses: false,
+        removeUnusedImports: true,
+    )
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        earlyReturn: true,
+    )
+    ->withSets([
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+        PHPUnitSetList::PHPUNIT_100,
+    ])
+    ->withRootFiles()
+    ->withSkip([
         AddLiteralSeparatorToNumberRector::class,
-        AddParamTypeFromPropertyTypeRector::class => [
-            __DIR__ . '/Classes/Code/MatomoMethodCall.php',
-        ],
-        AddReturnTypeDeclarationFromYieldsRector::class => [
-            __DIR__ . '/Tests/*',
-        ],
         PreferPHPUnitThisCallRector::class,
-        ReplaceTestAnnotationWithPrefixedFunctionRector::class,
-        TypedPropertyFromAssignsRector::class => [
-            __DIR__ . '/Classes/Event/TrackSiteSearchEvent.php',
-        ],
     ]);
-};
