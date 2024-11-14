a8db36130690cdc1eb8820932715ac19324b1a87
Mathias Brodala <mbrodala@pagemachine.de>
2024-11-14 11:16:25 +0100

    [TASK] Test that protocol-relative URLs are accepted

--- Tests/Unit/Hooks/PageRenderer/TrackingCodeInjectorTest.php
+++ Tests/Unit/Hooks/PageRenderer/TrackingCodeInjectorTest.php
@@ -285,6 +285,43 @@ final class TrackingCodeInjectorTest extends TestCase
     }
 
     #[Test]
+    public function executeAcceptsUrlWithRelativeProtocol(): void
+    {
+        $this->configureDefaultRequestStubForFrontend();
+
+        $this->pageRendererMock
+            ->expects(self::once())
+            ->method('addHeaderData')
+            ->with('<script>/* some tracking code */</script>');
+        $this->pageRendererMock
+            ->expects(self::never())
+            ->method('addFooterData');
+
+        $configuration = [
+            'matomoIntegrationUrl' => '//example.org/',
+            'matomoIntegrationSiteId' => 42,
+        ];
+
+        $this->siteStub
+            ->method('getConfiguration')
+            ->willReturn($configuration);
+
+        $this->javaScriptTrackingCodeBuilderStub
+            ->method('getTrackingCode')
+            ->willReturn('/* some tracking code */');
+
+        $subject = new TrackingCodeInjector(
+            $this->javaScriptTrackingCodeBuilderStub,
+            $this->noScriptTrackingCodeBuilderStub,
+            $this->tagManagerCodeBuilderStub,
+            $this->scriptTagBuilder,
+            new NoopEventDispatcher(),
+        );
+        $params = [];
+        $subject->execute($params, $this->pageRendererMock);
+    }
+
+    #[Test]
     public function ensureModifySiteConfigurationEventIsDispatched(): void
     {
         $this->configureDefaultRequestStubForFrontend();
